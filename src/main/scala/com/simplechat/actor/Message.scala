fbe8956ddf657c1dbb10045be24e666d643a802a
Elizer Dela Cruz <elizer.delacruz@mnltechnology.com>
2020-05-05 16:20:49 +0800

    Fixing the UI

--- src/main/scala/com/simplechat/actor/Message.scala
+++ src/main/scala/com/simplechat/actor/Message.scala
@@ -1,7 +1,9 @@
 package com.simplechat.actor
 
+import com.simplechat.protocol.ChatProtocol.InfoMessage
+
 object Message {
-  def joined(username: String) = s"$username joined the chat"
-  def quit(username: String) = s"$username quit the chat"
-  def reconnected(username: String) = s"$username reconnected the chat"
+  def joined(username: String) = InfoMessage(s"$username joined the chat")
+  def quit(username: String) = InfoMessage(s"$username quit the chat")
+  def reconnected(username: String) = InfoMessage(s"$username reconnected the chat")
 }
